0c5145e8e16682919fb8a134ed2442dad01c5119
EC2 Default User <ec2-user@ip-172-31-23-156.ap-northeast-1.compute.internal>
2020-09-26 06:22:35 +0000

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Question.create(id: 1, name: 'Test Taro', title: 'Test title 1', content: 'Test content 1')
+Question.create(id: 2, name: 'Test Sato', title: 'Test title 2', content: 'Test content 2')
+Question.create(id: 3, name: 'Test Oota', title: 'Test title 3', content: 'Test content 3')
